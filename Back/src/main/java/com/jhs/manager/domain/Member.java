7863728ca01ebb0582c5f0b07ba29710c85b042c
wnghdtjr129 <wnghdtjr129@naver.com>
2022-07-26 16:44:23 +0900

    [Create] 22.07.26-domain model 생성

---
+++ Back/src/main/java/com/jhs/manager/domain/Member.java
@@ -0,0 +1,62 @@
+package com.jhs.manager.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Persistable;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member implements Persistable<String> {
+
+    @Id
+    private String id;
+    private String name;
+    private String password;
+    private int age;
+    private int salary;
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name="TEAM_ID")
+    private Team team;
+
+    @Builder
+    public Member(String id, String name, String password, int age, int salary, Team team) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.age = age;
+        this.salary = salary;
+        this.team = team;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdAt == null;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+
+    public void changeSalary(int salary){
+        this.salary = salary;
+    }
+
+    public void setTeam(Team team){
+        if (this.team == null) {
+            this.team = team;
+            team.getMembers().add(this);
+        }
+    }
+}
