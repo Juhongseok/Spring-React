292d698a6279d9b611280c58326b2bbf9a56bc5b
wnghdtjr129 <wnghdtjr129@naver.com>
2022-07-26 17:34:23 +0900

    [Modify] 22.07.26-기본 화면 스타일 변경

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import DefaultHeader from './DefaultHeader.js';
+import LoginHeader from './LoginHeader.js';
 import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  var [login, setLogin] = React.useState(false);
+
+  if(login){
+    return (
+      <div className="App">
+          <LoginHeader/>
+      </div>
+    );
+  }else{
+    return (
+      <div className="App">
+          <DefaultHeader/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
