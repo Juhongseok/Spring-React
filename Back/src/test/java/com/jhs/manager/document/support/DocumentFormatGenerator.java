a29a190c3ecc1301b79b7950b3b3e940f2abf0b9
Juhongseok <wnghdtjr129@naver.com>
2022-08-17 15:08:19 +0900

    Revert "Revert "[Create] 22.08.11-설정 분리""
    This reverts commit e5392a2f446b3dc14ef513b897716c5bb13ca062.

---
+++ Back/src/test/java/com/jhs/manager/document/support/DocumentFormatGenerator.java
@@ -0,0 +1,9 @@
+package com.jhs.manager.document.support;
+
+import org.springframework.restdocs.snippet.Attributes;
+
+public interface DocumentFormatGenerator {
+    static Attributes.Attribute field(final String key, final String value){
+        return new Attributes.Attribute(key,value);
+    }
+}
